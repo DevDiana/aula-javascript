6c975fedd6238bbcd0410a934d1fa9fa9ec44995
diana <devdianaregina@gmail.com>
2020-09-08 19:07:18 -0300

    enviando mais sobre as aulas

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 let nome = 'Diana';//string literal
 let idade = 25; // number literal
+let estaAprovado = true // boolean
+let sobrenome = undefined;
+let corSelecionado = null // redefinir um valor
+
+// tipagem dinamica
+
+

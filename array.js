f5974a326c36556b90e5cf01c005c9f13882094a
diana <devdianaregina@gmail.com>
2020-09-08 23:50:11 -0300

    tipos de funções

--- array.js
+++ array.js
@@ -18,4 +18,23 @@ console.log(corSite);
 resetaCor("verde", " claro");
 console.log(corSite);
 
+// Tipos de funções
+//função que realiza a tarefa e nao devolve nada
 
+function dizerNome(){
+    console.log('Diana');
+
+}
+dizerNome();
+
+
+// Faz um cálculo ou operação e retorna algo
+function MultiplicarPorDois(valor){
+    return valor* 2;
+
+}
+//console.log(MultiplicarPorDois(5));
+
+let resultado = MultiplicarPorDois(5);
+
+console.log(resultado);
